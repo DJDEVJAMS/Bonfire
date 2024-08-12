e024f0664a4f954f7f07b55fd700ad59af3eb0be
DJDEVJAMS <alex.art.brown@gmail.com>
2024-08-12 15:05:04 -0500

    update routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const {Comment} = require("../../models");
+const {Comment,User,Hobby} = require("../../models");
+
+// GET all comments
 
 router.get('/', async (req, res) => {
     try {
@@ -13,18 +15,48 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Failed to load comments' });
     }
 });
+// // Get all comments linked to a post
+// router.get('/:id', async (req, res) => {
+//     try {
+//         // Example: Fetch all comments
+//         const comments = await Comment.findByPk();
+//         console.log(comments);
+//         // res.render('all', { comments });
+//         res.status(200).json(comments); // Pass comments to the template
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({ error: 'Failed to load comments' });
+//     }
+// });
+
+router.get('/:id', async (req, res) => {
+    try {
+        // Example: Fetch all comments
+        const comments = await Comment.findByPk(req.params.id);
+        console.log(comments);
+        // res.render('all', { comments });
+        res.status(200).json(comments); // Pass comments to the template
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to load comments' });
+    }
+});
+
+// POST a comment linked to a post
 
 router.post('/', async (req, res) => {
     try {
         const comData = await Comment.create({
             message: req.body.message,
-            timestamp: req.body.timestamp,
-            user_id: req.session.user_id
+            user_id: req.session.user_id,
+            post_id: req.body.post_id,
         });
         res.status(200).json(comData);
     } catch (err) {
         res.status(400).json(err);
     }
 });
+
+
 
 module.exports = router;
