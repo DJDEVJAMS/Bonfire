e7adb458f8cba5719a0d8dd9927ce75f4f476094
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-08-13 13:25:09 -0500

    got login to work correctly and use the correct pathing

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,13 @@ const router = require("express").Router();
 const homeRoutes = require("./homeRoutes");
 const apiRoutes = require("./api/");
 const pageOneRoutes = require("./pageOneRoutes");
-// const userRoutes = require(".userRoutes");
+// const userRoutes = require("./api/userRoutes");
 
 // Connect the routes to the router here
+
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+// router.use("/users",userRoutes);
 router.use("/pageOne", pageOneRoutes);
 
 module.exports = router;
